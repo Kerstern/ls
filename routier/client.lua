2149adf0422361cff230606040102fb22f475ff6
souzen13 <baptiste.franel@outlook.fr>
2017-04-27 10:33:41 +0200

    Update client.lua

--- routier/client.lua
+++ routier/client.lua
@@ -1,33 +1,23 @@
--- Position de la compagnie de routier
-local trucker_locations = {
-	{name="Mission camions", id=162, x= 814.87, y= -1664.11, z= 29.38},
-	{name="Mission camions", id=162, x= 814.87, y= -1664.11, z= 29.38},
-	{name="Mission camions", id=162, x= 814.87, y= -1664.11, z= 29.38},
-}
-
-
 -- Missions Routier
 local MissionRoutier = {
 	
-	['mission1'] = {
-		["name"] = "Mission 1",
-		["start"] = {['x'] = 814.879, ['y'] = -1664.115, ['z'] = 29.381},
-		["end"] = {['x'] = -654.597, ['y'] = 308.810, ['z'] = 82.900},
-		["payment"] = 1500
-		},
-	
-	['mission2'] = {
-		["name"] = "Mission 2",
-		["start"] = {['x'] = 814.879, ['y'] = -1664.115, ['z'] = 29.381},
-		["end"] = {['x'] = -1092.924, ['y'] = -2052.076, ['z'] = 13.291},
-		["payment"] = 1500
-		},
-	
-	['mission3'] = {
-		["name"] = "Mission 3",
-		["start"] = {['x'] = 814.879, ['y'] = -1664.115, ['z'] = 29.381},
-		["end"] = {['x'] = 2354.301, ['y'] = 3133.806, ['z'] = 48.208},
-		["payment"] = 2500
-		},
-}
+		[0] = {-654.597, 308.810, 82.900, 1500}, --x,y,z,money
+		[1] = {-1092.924, -2052.076, 13.291, 1500},
+		[2] = {2354.301, 3133.806, 48.208, 2500},
+	}
 
+
+-- Ajouteur blip dans Blips
+
+local MISSION = {}
+MISSION.start = false
+MISSION.tailer = false
+MISSION.truck = false
+
+MISSION.hashTruck = 0
+MISSION.hashTrailer = 0
+
+local currentMission = -1
+
+local playerCoords
+local playerPed
