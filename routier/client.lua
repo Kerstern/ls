9e2eb836d61752a868c182fe3d4db77218a5b0d1
souzen13 <baptiste.franel@outlook.fr>
2017-04-27 10:44:47 +0200

    Update client.lua

--- routier/client.lua
+++ routier/client.lua
@@ -7,17 +7,49 @@ local MissionRoutier = {
 	}
 
 
--- Ajouteur blip dans Blips
+local routier = {
+  {name="Truck & Co", colour=15, id=68, x=-266.94, y=-960.744, z=31.2231},
+}
 
-local MISSION = {}
-MISSION.start = false
-MISSION.tailer = false
-MISSION.truck = false
+---------------------------------- FUNCTIONS ----------------------------------
 
-MISSION.hashTruck = 0
-MISSION.hashTrailer = 0
+function drawTxt(text,font,centre,x,y,scale,r,g,b,a)
+  SetTextFont(font)
+  SetTextProportional(0)
+  SetTextScale(scale, scale)
+  SetTextColour(r, g, b, a)
+  SetTextDropShadow(0, 0, 0, 0,255)
+  SetTextEdge(1, 0, 0, 0, 255)
+  SetTextDropShadow()
+  SetTextOutline()
+  SetTextCentre(centre)
+  SetTextEntry("STRING")
+  AddTextComponentString(text)
+  DrawText(x , y)
+end
 
-local currentMission = -1
+function IsNearJobs()
+  local ply = GetPlayerPed(-1)
+  local plyCoords = GetEntityCoords(ply, 0)
+  for _, item in pairs(changeYourJob) do
+    local distance = GetDistanceBetweenCoords(item.x, item.y, item.z,  plyCoords["x"], plyCoords["y"], plyCoords["z"], true)
+    if(distance <= 10) then
+      return true
+    end
+  end
+end
 
-local playerCoords
-local playerPed
+function menuJobs()
+  MenuTitle = "Jobs"
+  ClearMenu()
+  for _, item in pairs(jobs) do
+    local nameJob = item.name
+    local idJob = item.id
+    Menu.addButton(nameJob,"changeJob",idJob)
+  end
+end
+
+function changeJob(id)
+  TriggerServerEvent("jobssystem:jobs", id)
+end
+
