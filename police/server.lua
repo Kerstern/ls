a2a05600c012e44095d3d39d20d95f455af4aebd
souzen13 <baptiste.franel@outlook.fr>
2017-04-27 14:11:09 +0200

    Create server.lua

---
+++ police/server.lua
@@ -0,0 +1,21 @@
+RegisterServerEvent('cp:spawncheck')
+AddEventHandler('cp:spawncheck', function()
+
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+
+  local player = user.identifier
+  
+  if user.permission_level == (2) then
+  
+  TriggerClientEvent('policespawn', source)
+  
+  TriggerClientEvent('worked', source)
+  
+  else
+  
+  TriggerClientEvent('notworked', source)
+
+
+  end
+ end)
+end)
