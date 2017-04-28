d61b5317c32df3caa552a11e0e18a663feb01e2b
souzen13 <baptiste.franel@outlook.fr>
2017-04-28 11:20:57 +0200

    Update sv_permis.lua

--- permis/sv_permis.lua
+++ permis/sv_permis.lua
@@ -1,1 +1,19 @@
+require "resources/essentialmode/lib/MySQL"
+MySQL:open("localhost", "gta5_gamemode_essential", "root", "password")
 
+
+function updatelicence(player, id)
+  local licence = id
+  MySQL:executeQuery("UPDATE users SET `licencea`='@value' WHERE identifier = '@identifier'", {['@value'] = licence, ['@identifier'] = player})
+end
+
+
+RegisterServerEvent('permis:licence')
+AddEventHandler('permis:licence', function(id)
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+        local player = user.identifier
+        local namePermis = namePermis(id)
+        updatelicence(player, id)
+        TriggerClientEvent("es_freeroam:notify", source, "CHAR_MP_STRIPCLUB_PR", 1, "Auto-Ecole", false, "Félicitations, vous avez votre permis !")
+  end)
+end)
